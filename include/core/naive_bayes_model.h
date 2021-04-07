f6e62e7399ce1266676f896054367977f2b2ce26
nvacharya2 <42473577+nvacharya2@users.noreply.github.com>
2021-04-07 03:07:44 -0500

    FindClassFrequencies() done, working on TrainModel().

--- include/core/naive_bayes_model.h
+++ include/core/naive_bayes_model.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include "image.h"
 #include "image_processor.h"
 
@@ -9,7 +10,6 @@ namespace naivebayes {
 class Model {
 
   public:
-    //std::string GetBestClass() const; //DELETE (in src too)
 
     //calculate probabilities for each class
     //calculate conditional probabilities o a pixel being (un)shaded
@@ -19,24 +19,34 @@ class Model {
     Model();
     Model(ImageProcessor &processor);
 
-    std::vector<int> GetImageClasses();
-    std::vector<Image> GetTrainingImages();
+    void TrainModel(int k);
+
+    void FindClassFrequencies();
+
+    void CalculateClassProbability();
+
+    void CalculatePixelProbability();
 
     void LoadModel();
+
     void SaveModel();
-    void CalculateClassProbability();
-    void CalculateShadeProbability();
 
-
-
-
-
+    /*
+  std::vector<int> GetImageClasses();
+  std::vector<Image> GetTrainingImages();
+   */
   private:
 
-    int image_width;
+    int image_width_;
+    int k_ = 1; //laplace smoothing
+    int v_;
+
 
     std::vector<Image> training_images_;
     std::vector<int> image_classes_;
+
+    std::unordered_map<int, int> class_frequency_;
+
 
     //std::vector<double> class_chance_;
     //std::vector<double> pixel_shade_chance_;
@@ -46,15 +56,3 @@ class Model {
 };
 
 }  // namespace naivebayes
-
-/*
-TODO: rename this file. You'll also need to modify CMakeLists.txt.
-
-You can (and should) create more classes and files in include/core (header
- files) and src/core (source files); this project is too big to only have a
- single class.
-
-Make sure to add any files that you create to CMakeLists.txt.
-
-TODO Delete this comment before submitting your code.
-*/
--- src/core/naive_bayes_model.cc
+++ src/core/naive_bayes_model.cc
@@ -2,10 +2,7 @@
 
 namespace naivebayes {
 
-/*std::string Model::GetBestClass() const { //DELETE
-  return "CS 126";
-}*/
-//Model::Model() {}
+Model::Model() {}
 
 Model::Model(ImageProcessor &processor) {
 
@@ -21,8 +18,38 @@ Model::Model(ImageProcessor &processor) {
     //sort(image_classes_.begin(), image_classes_.end()); //std::sort?
      */
 
-    image_width = training_images_[0].GetLength(); //image dimensions
+    image_width_ = training_images_[0].GetLength(); //image dimensions
 }
+
+
+//use unordered map for storing class probability?? less run time with large amounts of data
+void Model::TrainModel(int k) {
+    k_ = k;
+
+    FindClassFrequencies(); //should work - done?
+
+        // (k_ + class_frequency_[c]) / (10k_ + training_images_.size())
+        // (# of classes = 10 --> v_)
+    CalculateClassProbability();
+
+    CalculatePixelProbability();
+}
+
+
+void Model::FindClassFrequencies() {
+    for (Image &image : training_images_) {
+        class_frequency_[image.GetLabel()]++; //updates #times label is found w/ label as key
+    }
+}
+
+void Model::CalculateClassProbability() {}
+
+void Model::CalculatePixelProbability() {}
+
+
+void Model::LoadModel() {}
+
+void Model::SaveModel() {}
 
 /*
 std::vector<int> Model::GetImageClasses() {
@@ -32,16 +59,5 @@ std::vector<int> Model::GetImageClasses() {
 std::vector<Image> Model::GetTrainingImages() {
     return training_images_;
 }*/
-
-//use unordered map for storing class probability?? less run time with large amounts of data
-
-void Model::CalculateClassProbability() {}
-
-void Model::CalculateShadeProbability() {}
-
-
-void Model::LoadModel() {}
-
-void Model::SaveModel() {}
 
 }  // namespace naivebayes
